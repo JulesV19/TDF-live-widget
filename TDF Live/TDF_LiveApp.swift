8a3d68114a2fdc516876b1fb3f8c6b7ae3c9caf6
JulesV <jules.videgrain@student-cs.fr>
2026-07-08 13:05:05 -0300

    Initial Commit

---
+++ TDF Live/TDF_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDF_LiveApp.swift
+//  TDF Live
+//
+//  Created by Jules Videgrain on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TDF_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
